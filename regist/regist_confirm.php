597f1c90018b46b585929a94253398cfb504b286
waka-di <miho.1225.miho@icloud.com>
2025-10-13 21:31:07 +0900

    登録確認画面php作成開始

---
+++ regist/regist_confirm.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<title>アカウント登録確認</title>
+
+<!-- Google Fonts -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&display=swap" rel="stylesheet">
+
+<!-- リセットCSS -->
+<link rel="stylesheet" href="https://unpkg.com/ress@4.0.0/dist/ress.min.css">
+<!-- Bootstrap Icons -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+
+<link rel="stylesheet" href="../css/style.css">
+
+</head>
+
+<body>
+<!-- ヘッダー -->
+<header class="header">
+    <div class="logo">
+            <img src="../images/logo.png" alt="WHO’S ToDo List">
+    </div>
+</header>
+
+<!-- メイン -->
+<main class="regist-confirm">
+    <div class="regist-confirm-container">
+            
+    </div>
+</main>
+
+<!-- フッター -->
+<footer class="footer">
+    <div class="container">
+        <small class="footer-copyright">Copyright © 2025 M/W's Portfolio. All Rights Reserved.</small>
+    </div>
+</footer>
+
+<!-- jQuery -->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<!-- Slick -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js"></script>
+<script src="../js/main.js"></script>
+</body>
+</html>
