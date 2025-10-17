84d3d3b39246a64e940ca60c0fc4301123b9cd79
waka-di <miho.1225.miho@icloud.com>
2025-10-17 22:06:05 +0900

    userDTOカラム名DBに合わせ訂正

--- dto/userDTO.php
+++ dto/userDTO.php
@@ -1,28 +1,34 @@
 <?php
 class User {
-    private $id;
-    private $name;
-    private $email;
+    private $user_id;
+    private $user_name;
+    private $mail;
     private $password;
+    private $insert_date;
 
     // コンストラクタ
-    public function __construct($id = null, $name = '', $email = '', $password = '') {
-        $this->id = $id;
-        $this->name = $name;
-        $this->email = $email;
+    public function __construct($user_id = null, $user_name = '', $mail = '', $password = '', $insert_date = '') {
+        $this->user_id = $user_id;
+        $this->user_name = $user_name;
+        $this->mail = $mail;
         $this->password = $password;
+        $this->insert_date = $insert_date;
     }
 
     // getter / setter
-    public function getId() { return $this->id; }
-    public function setId($id) { $this->id = $id; }
+    public function getUserId() { return $this->user_id; }
+    public function setUserId($user_id) { $this->user_id = $user_id; }
 
-    public function getName() { return $this->name; }
-    public function setName($name) { $this->name = $name; }
+    public function getUserName() { return $this->user_name; }
+    public function setUserName($user_name) { $this->user_name = $user_name; }
 
-    public function getEmail() { return $this->email; }
-    public function setEmail($email) { $this->email = $email; }
+    public function getMail() { return $this->mail; }
+    public function setMail($mail) { $this->mail = $mail; }
 
     public function getPassword() { return $this->password; }
     public function setPassword($password) { $this->password = $password; }
+
+    public function getInsertDate() { return $this->insert_date; }
+    public function setInsertDate($insert_date) { $this->insert_date = $insert_date; }
 }
+?>
