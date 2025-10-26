6ec2c3954875b866def9530ad527b137bb4ba0fe
waka-di <miho.1225.miho@icloud.com>
2025-10-26 10:38:46 +0900

    ログアウトコード整理

--- logout.php
+++ logout.php
@@ -3,15 +3,10 @@ session_start();
 
 $_SESSION = [];
 
-if (ini_get("session.use_cookies")) {
-    $params = session_get_cookie_params();
-    setcookie(session_name(), '', time() - 42000,
-        $params["path"], $params["domain"],
-        $params["secure"], $params["httponly"]
-    );
-}
-
 session_destroy();
+
+if (ini_get("session.use_cookies")) {
+    setcookie(session_name(), '', time() - 1, '/');
 
 header("Location: index.php");
 exit;
